5be60216858ca4a4dcc217be2d386e34b1c30246
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-05-19 11:15:35 +0700

    deletenode

--- 162_singlelinkedlist.cpp
+++ 162_singlelinkedlist.cpp
@@ -38,7 +38,7 @@ void addNode() {
 				return;
 			}
 
-			3previous = current;
+			previous = current;
 			current = current->next;
 		}
 
@@ -50,5 +50,25 @@ void addNode() {
 bool searchNode(int nim, Node* current, Node* previous) {
 	previous = START;
 	current = START;
-	while (current != NULL && nim > current)
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL) {
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool deleteNode(int nim) {
+
 }
